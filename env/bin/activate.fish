b9ca80699ebe65978e5aa601f4c82eae4824afdd
Vishal Polley <vishalpolley290996@gmail.com>
2018-08-14 10:05:42 +0530

    Uploaded revised codes

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vishalpolley/Documents/lpr/env"
+set -gx VIRTUAL_ENV "/home/vishalpolley/Desktop/lpr/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- env/lib/python3.5/struct.py
+++ env/lib/python3.5/struct.py
@@ -1,15 +1,1 @@
-__all__ = [
-    # Functions
-    'calcsize', 'pack', 'pack_into', 'unpack', 'unpack_from',
-    'iter_unpack',
-
-    # Classes
-    'Struct',
-
-    # Exceptions
-    'error'
-    ]
-
-from _struct import *
-from _struct import _clearcache
-from _struct import __doc__
+/usr/lib/python3.5/struct.py
